645876dc4024d8fd97bbfe1c162d371d5879c35d
kaiorod <caiorodrigo2775@gmail.com>
2021-08-22 17:23:21 -0300

    finaliza MemeToNervoso

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MemeToNervoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+            Console.Beep();
+            Console.Clear();
+
+            Console.Write("O que você pensa sobre mim? : ");
+            texto = Console.ReadLine();
+
+            Console.Beep();
+            Console.WriteLine("E sabe o que eu penso de você?");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"{texto.Remove(10)}... NÃO PERA. Tô nervoso");
+            Console.ResetColor();
+
+        }
+    }
+}
